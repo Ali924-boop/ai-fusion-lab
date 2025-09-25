aead48d243bab534273706b58764c7becd245c15
Ali Raza <alirazasandha5@gmail.com>
2025-09-25 08:19:22 -0700

    Added Layout and Multi Models

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import { AppSidebar } from './_components/AppSidebar'
+"use client";
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./_components/AppSidebar";
+import AppHeader from "./_components/AppHeader"
 
-function Provider({
-   children,
-   ...props 
-}) {
+function Provider({ children, ...props }) {
   return (
-    <NextThemesProvider 
-    attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            {...props}>
-              <SidebarProvider>
-                <AppSidebar />
-                <SidebarTrigger />
-    <div>
-      {children}</div>
+    <NextThemesProvider
+      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <SidebarProvider>
+        <AppSidebar />
+           
+        <div className="w-full">
+          <AppHeader/>
+          {children}</div>
       </SidebarProvider>
     </NextThemesProvider>
-  )
+  );
 }
 
-export default Provider
+export default Provider;
